7ce5d625e943f8b5ec5f7eba2750bad3d56b04c4
prathamRT01 <prathamthakkar1998@gmail.com>
2024-05-15 19:53:09 +0530

    complete profile page UI

--- client/src/Components/OAuth.jsx
+++ client/src/Components/OAuth.jsx
@@ -27,7 +27,7 @@ export default function OAuth() {
             })
             console.log(res);
         const data = await res.json();
-        // console.log(data);
+        console.log(data);
         dispatch(signInSuccess(data));
         navigate('/');
 
--- client/src/pages/tp.jsx
+++ client/src/pages/tp.jsx
@@ -2,7 +2,6 @@ import { useSelector } from 'react-redux';
 
 export default function Profile() {
   const {currentUser} = useSelector((state) => state.user);
-  console.log('currentUser:', currentUser);
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -14,10 +13,6 @@ export default function Profile() {
       <input type="text" placeholder='password' id='password' className='border p-3 rounded-lg'/>
       <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
       </form>
-      <div  className=' flex justify-between mt-5'>
-      <span className='text-red-700 cursor-pointer'>Delete Account</span>
-      <span className='text-red-700 cursor-pointer'>SignOut</span>
-      </div>
     </div>
   )
 }
