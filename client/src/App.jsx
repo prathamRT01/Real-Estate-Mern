9db234092cc70c5539785d59956511ab8bcb57a0
prathamRT01 <prathamthakkar1998@gmail.com>
2024-05-24 04:05:52 +0530

    Add image slide to listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import PrivateRoute from "./Components/PrivateRoute.jsx";
 import CreateListing from "./pages/CreateListing.jsx";
 import UpdateListing from "./pages/UpdateListing.jsx";
+import Listing from "./pages/Listing.jsx";
 
 export default function App() {
   return <BrowserRouter>
@@ -17,6 +18,7 @@ export default function App() {
     <Route path="/sign-in"element={<SignIn/>}/>
     <Route path="/sign-up"element={<SignUp/>}/>
     <Route path="/about"element={<About/>}/>
+    <Route path="/listing/:listingId"element={<Listing/>}/>
     <Route element={<PrivateRoute/>}>
     <Route path="/profile"element={<Profile/>}/>
     <Route path="/create-listing"element={<CreateListing/>}/>
